2d6469fa86f07c38023b2e2b12e8f2b455c29591
Morgan Van Amerongen <mistigri.van@gmail.com>
2020-12-15 16:22:51 +0100

    first day

---
+++ 2019/1/1.rs
@@ -0,0 +1,50 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+
+    let file = File::open("1.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+
+    let mut input = vec![];
+    for l in lines {
+        if let Ok(l) = l {
+            input.push(l.parse::<i32>().unwrap());
+        }
+    }
+
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+fn part_1(input: &Vec<i32>) -> f32 {
+
+    let mut sum = 0.0;
+
+    for mass in input.iter() {
+        sum += (*mass as f32 / 3 as f32).floor() - 2 as f32;
+    }
+
+    sum
+}
+
+fn part_2(input: &Vec<i32>) -> f32 {
+
+    let mut sum = 0.0;
+
+    for mass in input.iter() {
+        let mut fuel = *mass as f32;
+        loop {
+            fuel = (fuel / 3.0).floor() - 2.0;
+
+            if fuel <= 0.0 {
+                break;
+            }
+
+            sum += fuel;
+        }
+    }
+
+    sum
+}
