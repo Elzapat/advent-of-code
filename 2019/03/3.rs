8cd75630c5bbd223ce9f56aabd3cdb5f95340063
Morgan Van Amerongen <mistigri.van@gmail.com>
2020-12-19 12:15:11 +0100

    2019 day three not finished

--- 2019/03/3.rs
+++ 2019/03/3.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 struct Instr {
     distance: i32,
@@ -33,6 +34,7 @@ fn main() {
     }
 
     println!("Part 1: {}", part_1(&mut first_wire, &mut second_wire));
+    println!("Part 2: {}", part_2(&mut first_wire, &mut second_wire));
 }
 
 fn manhattan_distance(p1: (i32, i32), p2: (i32, i32)) -> i32 {
@@ -40,74 +42,60 @@ fn manhattan_distance(p1: (i32, i32), p2: (i32, i32)) -> i32 {
     (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
 }
 
-fn compute_distance(instruction: &Instr) -> (i32, i32) {
+fn compute_path(instructions: &Vec<Instr>) -> HashSet<(i32, i32)> {
 
-    match instruction.direction {
-        'L' => return (-instruction.distance, 0),
-        'R' => return (instruction.distance, 0),
-        'D' => return (0, instruction.distance),
-        'U' => return (0, -instruction.distance),
-        _ => panic!("Invalid direction"),
+    let mut pos = (0, 0);
+    let mut wire_path = HashSet::new();
+
+    for instr in instructions.iter() {
+        for _ in 0..instr.distance {
+            match instr.direction {
+                'L' => pos.0 -= 1,
+                'R' => pos.0 += 1,
+                'D' => pos.1 += 1,
+                'U' => pos.1 -= 1,
+                _ => panic!("Invalid direction"),
+            }
+            wire_path.insert(pos);
+        }
     }
+
+    wire_path
 }
+
+fn computer_nb_steps(wire_path: &HashSet<(i32, i32)>, )
 
 fn part_1(first_wire: &mut Vec<Instr>, second_wire: &mut Vec<Instr>) -> i32 {
 
-    let mut intersections = vec![];
-    let mut first_wire_pos = vec![(0, 0)];
-    let mut second_wire_pos = vec![(0, 0)];
+    let first_wire_path = compute_path(&first_wire);
+    let second_wire_path = compute_path(&second_wire);
 
-    for i in 0..first_wire.len() {
-        let first_pos_diff = compute_distance(&first_wire[i]);
-        let second_pos_diff = compute_distance(&second_wire[i]);
-
-        let mut first_current_pos = first_wire_pos[first_wire_pos.len() - 1];
-        let mut second_current_pos = second_wire_pos[second_wire_pos.len() - 1];
-
-        let first_target_pos = (first_current_pos.0 + first_pos_diff.0, first_current_pos.1 + first_pos_diff.1);
-        let second_target_pos = (second_current_pos.0 + second_pos_diff.0, second_current_pos.1 + second_pos_diff.1);
-
-        while first_current_pos != first_target_pos {
-            first_wire_pos.push(first_current_pos);
-            if first_pos_diff.0 == 0 {
-                first_current_pos.1 += first_pos_diff.1 / first_pos_diff.1.abs();
-            } else {
-                first_current_pos.0 += first_pos_diff.0 / first_pos_diff.0.abs();
-            }
-        }
-
-        while second_current_pos != second_target_pos {
-            second_wire_pos.push(second_current_pos);
-            if first_pos_diff.0 == 0 {
-                second_current_pos.1 += second_pos_diff.1 / second_pos_diff.1.abs();
-            } else {
-                second_current_pos.0 += second_pos_diff.0 / second_pos_diff.0.abs();
-            }
-        }
-    }
-
-//    let intersections = first_wire_pos.intersect(second_wire_pos); 
-    
-    for first_pos in first_wire_pos.iter() {
-        for second_pos in second_wire_pos.iter() {
-            if first_pos == second_pos {
-                println!("pos: {:?}", first_pos);
-                intersections.push(*first_pos);
-            }
-        }
-    }
+    let intersections: HashSet<_> = first_wire_path.intersection(&second_wire_path).collect();
 
     let mut min_distance = 100_000_000;
     for intersection in intersections.iter() {
-        let distance = manhattan_distance((0, 0), *intersection);
-        if distance == 0 {
-            continue;
-        }
-        println!("distance: {}", distance);
+        let distance = manhattan_distance((0, 0), **intersection);
         if distance < min_distance {
-            min_distance = distance
+            min_distance = distance;
         }
     }
 
     min_distance
+}
+
+fn part_2(first_wire: &mut Vec<Instr>, second_wire: &mut Vec<Instr>) -> i32 {
+
+    let first_wire_path = compute_path(&first_wire);
+    let second_wire_path = compute_path(&second_wire);
+
+    let intersections: HashSet<_> = first_wire_path.intersection(&second_wire_path).collect();
+
+    let mut min_steps = 100_000_000;
+
+    for inter in intersections.iter() {
+        let mut first_wire_steps = 0;
+        let mut second_wire_step = 0;
+    }
+
+    min_steps
 }
