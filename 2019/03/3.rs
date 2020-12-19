d23bfcefd80815c71c1226bca38da2b461690491
Morgan Van Amerongen <mistigri.van@gmail.com>
2020-12-19 18:23:30 +0100

    2019 day 3

--- 2019/03/3.rs
+++ 2019/03/3.rs
@@ -63,12 +63,23 @@ fn compute_path(instructions: &Vec<Instr>) -> HashSet<(i32, i32)> {
     wire_path
 }
 
-fn computer_nb_steps(wire_path: &HashSet<(i32, i32)>, )
+fn compute_nb_steps(wire_path: &Vec<(i32, i32)>, intersection: &(i32, i32)) -> i32 { 
+
+    let mut nb_steps = 0;
+    for step in wire_path.iter() {
+        nb_steps += 1;
+        if *step == *intersection {
+            break;
+        }
+    }
+
+    nb_steps
+}
 
 fn part_1(first_wire: &mut Vec<Instr>, second_wire: &mut Vec<Instr>) -> i32 {
 
-    let first_wire_path = compute_path(&first_wire);
-    let second_wire_path = compute_path(&second_wire);
+    let first_wire_path: HashSet<_> = compute_path(&first_wire);
+    let second_wire_path: HashSet<_> = compute_path(&second_wire);
 
     let intersections: HashSet<_> = first_wire_path.intersection(&second_wire_path).collect();
 
@@ -85,17 +96,44 @@ fn part_1(first_wire: &mut Vec<Instr>, second_wire: &mut Vec<Instr>) -> i32 {
 
 fn part_2(first_wire: &mut Vec<Instr>, second_wire: &mut Vec<Instr>) -> i32 {
 
-    let first_wire_path = compute_path(&first_wire);
-    let second_wire_path = compute_path(&second_wire);
+    let first_wire_path = compute_path_vector(&first_wire);
+    let second_wire_path = compute_path_vector(&second_wire);
+    let first_wire_path_set = compute_path(&first_wire);
+    let second_wire_path_set = compute_path(&second_wire);
 
-    let intersections: HashSet<_> = first_wire_path.intersection(&second_wire_path).collect();
+    let intersections: HashSet<_> = first_wire_path_set.intersection(&second_wire_path_set).collect();
 
-    let mut min_steps = 100_000_000;
+    let mut min_nb_steps = 100_000_000;
 
     for inter in intersections.iter() {
-        let mut first_wire_steps = 0;
-        let mut second_wire_step = 0;
+        let first_nb_steps = compute_nb_steps(&first_wire_path, *inter);
+        let second_nb_steps = compute_nb_steps(&second_wire_path, *inter);
+
+        if first_nb_steps + second_nb_steps < min_nb_steps {
+            min_nb_steps = first_nb_steps + second_nb_steps;
+        }
     }
 
-    min_steps
+    min_nb_steps
+}
+
+fn compute_path_vector(instructions: &Vec<Instr>) -> Vec<(i32, i32)> {
+
+    let mut pos = (0, 0);
+    let mut wire_path = vec![];
+
+    for instr in instructions.iter() {
+        for _ in 0..instr.distance {
+            match instr.direction {
+                'L' => pos.0 -= 1,
+                'R' => pos.0 += 1,
+                'D' => pos.1 += 1,
+                'U' => pos.1 -= 1,
+                _ => panic!("Invalid direction"),
+            }
+            wire_path.push(pos);
+        }
+    }
+
+    wire_path
 }
