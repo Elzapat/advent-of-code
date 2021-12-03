671f67ae650280030c60008e393a26f50f3328ec
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2021-12-03 15:06:51 +0100

    optimized day 3

--- 2021/03/src/main.rs
+++ 2021/03/src/main.rs
@@ -1,109 +1,93 @@
+mod orig;
+
+use std::time::SystemTime;
+
+const N: usize = 12;
+
 fn main() {
-    let input = std::fs::read_to_string("input.txt")
+    let input = std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/input.txt"))
         .unwrap()
         .lines()
-        .map(|line| line.to_owned())
-        .collect::<Vec<_>>();
+        .map(|line| u32::from_str_radix(line, 2).unwrap())
+        .collect::<Vec<u32>>();
 
-    {
-        let mut ones = vec![];
-        let mut zeros = vec![];
+    orig::solve();
+    println!("--------------------------------------------");
 
-        let mut epsilon = 0_u32;
-        let mut gamma = 0_u32;
+    let mut start = SystemTime::now();
+    println!("Part 1: {}", part_1(input.clone()));
+    println!("Time part 1: {}ms", start.elapsed().unwrap().as_micros() as f32 / 1000.0);
 
-        for _ in 0..input[0].len() {
-            ones.push(0);
-            zeros.push(0);
+    start = SystemTime::now();
+    println!("Part 2: {}", part_2(input));
+    println!("Time part 2: {}ms", start.elapsed().unwrap().as_micros() as f32 / 1000.0);
+}
+
+fn part_1(input: Vec<u32>) -> u32 {
+    let mut gamma = 0;
+    let mut epsilon = 0;
+
+    let mut ones = [0; N];
+
+    for x in &input {
+        for i in (0..N).rev() {
+            ones[N - i - 1] += (x & (1 << i) > 0) as u32;
         }
-
-        for line in &input {
-            for (i, c) in line.chars().enumerate() {
-                match c {
-                    '1' => ones[i] += 1,
-                    _ => zeros[i] += 1,
-                } 
-            }
-        }
-
-        for i in 0..input[0].len() {
-            if ones[i] < zeros[i] {
-                // epsilon += (2.0_f32).powi(i as i32) as u32;
-                epsilon += 1;
-                // gamma <<= 1;
-            } else {
-                gamma += 1;
-                // gamma += (2.0_f32).powi(i as i32) as u32;
-                // epsilon <<= 1;
-            }
-            epsilon <<= 1;
-            gamma <<= 1;
-        }
-        epsilon >>= 1;
-        gamma >>= 1;
-
-        println!("Part 1: {}", gamma * epsilon);
     }
 
-    {
-        let mut o2 = input.clone();
-        let mut co2 = input.clone();
-
-        let mut ones_o2 = vec![];
-        let mut zeros_o2 = vec![];
-
-        let mut ones_co2 = vec![];
-        let mut zeros_co2 = vec![];
-
-        let mut i = 0;
-        while o2.len() > 1 || co2.len() > 1 {
-            for _ in 0..o2[0].len() {
-                ones_o2.push(0);
-                zeros_o2.push(0);
-            }
-
-            for _ in 0..co2[0].len() {
-                ones_co2.push(0);
-                zeros_co2.push(0);
-            }
-
-            for line in &o2 {
-                for (j, c) in line.chars().enumerate() {
-                    match c {
-                        '1' => ones_o2[j] += 1,
-                        _ => zeros_o2[j] += 1,
-                    } 
-                }
-            }
-
-            for line in &co2 {
-                for (j, c) in line.chars().enumerate() {
-                    match c {
-                        '1' => ones_co2[j] += 1,
-                        _ => zeros_co2[j] += 1,
-                    } 
-                }
-            }
-
-            if ones_o2[i] >= zeros_o2[i] {
-                if o2.len() > 1 { o2.retain(|line| line.chars().nth(i).unwrap() == '1'); }
-            } else {
-                if o2.len() > 1 { o2.retain(|line| line.chars().nth(i).unwrap() == '0'); }
-            }
-
-            if ones_co2[i] >= zeros_co2[i] {
-                if co2.len() > 1 { co2.retain(|line| line.chars().nth(i).unwrap() == '0') };
-            } else {
-                if co2.len() > 1 { co2.retain(|line| line.chars().nth(i).unwrap() == '1') };
-            }
-
-            i += 1;
-            ones_o2 = vec![];
-            zeros_o2 = vec![];
-            ones_co2 = vec![];
-            zeros_co2 = vec![];
+    for n in ones {
+        if n >= (input.len() / 2) as u32 {
+            gamma += 1;
+        } else {
+            epsilon += 1;
         }
 
-        println!("Part 2: {}", u32::from_str_radix(&o2[0], 2).unwrap() * u32::from_str_radix(&co2[0], 2).unwrap());
+        gamma <<= 1;
+        epsilon <<= 1;
     }
+    gamma >>= 1;
+    epsilon >>= 1;
+
+    gamma * epsilon
+}
+
+fn part_2(input: Vec<u32>) -> u32 {
+    let mut o = input.clone();
+    let mut co2 = input;
+
+    let mut i = 0;
+    while o.len() > 1 {
+        let mut ones = 0;
+
+        for x in &o {
+            ones += (*x & (1 << (N - 1 - i)) > 0) as u32;
+        }
+
+        if ones >= o.len() as u32 - ones {
+            o.retain(|x| (*x & (1 << (N - 1 - i))) > 0);
+        } else {
+            o.retain(|x| *x & (1 << (N - 1 - i)) == 0);
+        }
+
+        i += 1;
+    }
+
+    let mut i = 0;
+    while co2.len() > 1 {
+        let mut ones = 0;
+
+        for x in &co2 {
+            ones += (*x & (1 << (N - 1 - i)) > 0) as u32;
+        }
+
+        if ones >= co2.len() as u32 - ones {
+            co2.retain(|x| *x & (1 << (N - 1 - i)) == 0);
+        } else {
+            co2.retain(|x| *x & (1 << (N - 1 - i)) > 0);
+        }
+
+        i += 1;
+    }
+
+    o[0] * co2[0]
 }
