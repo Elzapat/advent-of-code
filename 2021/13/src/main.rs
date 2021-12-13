a76b8c7b4c0167947017190ed28e8710063da848
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2021-12-13 06:32:08 +0100

    2021 day 13

--- 2021/13/src/main.rs
+++ 2021/13/src/main.rs
@@ -1,3 +1,78 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let mut paper = [[false; 2000]; 2000];
+    let mut folds = vec![];
+
+    for line in input.lines() {
+        if line.contains("fold") {
+            let mut parts = line.split("=");
+            let axis = parts.next().unwrap().chars().last().unwrap();
+            let number = parts.next().unwrap().parse::<usize>().unwrap();
+            folds.push((axis, number));
+        } else if line.contains(",") {
+            let mut coords = line.split(",");
+            let x = coords.next().unwrap().parse::<usize>().unwrap();
+            let y = coords.next().unwrap().parse::<usize>().unwrap();
+            paper[x][y] = true;
+        }
+    }
+
+    {
+        let mut paper = paper.clone();
+        let folds = folds.clone();
+
+        if folds[0].0 == 'x' {
+            for i in (folds[0].1 + 1)..paper.len() {
+                for j in 0..paper[i].len() {
+                    if paper[i][j] {
+                        paper[folds[0].1 - (i - folds[0].1)][j] = true;
+                        paper[i][j] = false;
+                    }
+                }
+            }
+        } else {
+            for i in 0..paper.len() {
+                for j in (folds[0].1 + 1)..paper[i].len() {
+                    if paper[i][j] {
+                        paper[i][folds[0].1 - (j - folds[0].1)] = true;
+                        paper[i][j] = false;
+                    }
+                }
+            }
+        }
+
+        println!("Part 1: {}", paper.iter().fold(0, |acc, row| acc + row.iter().fold(0, |acc, p| acc + *p as u32)));
+    }
+
+    {
+        for fold in folds {
+            if fold.0 == 'x' {
+                for i in (fold.1 + 1)..paper.len() {
+                    for j in 0..paper[i].len() {
+                        if paper[i][j] {
+                            paper[ fold.1 - (i - fold.1)][j] = true;
+                            paper[i][j] = false;
+                        }
+                    }
+                }
+            } else {
+                for i in 0..paper.len() {
+                    for j in (fold.1 + 1)..paper[i].len() {
+                        if paper[i][j] {
+                            paper[i][fold.1 - (j - fold.1)] = true;
+                            paper[i][j] = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        println!("Part 2:");
+        for i in 0..6 {
+            for j in 0..39 {
+                print!("{}", if paper[j][i] { '#' } else { '.' });
+            }
+            println!("");
+        }
+    }
 }
