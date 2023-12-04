4b9532a1fe3301f257b7ab0e67201b881909bd09
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2023-12-04 08:46:52 +0100

    2023 day 04 part 1

---
+++ 04/src/main.rs
@@ -0,0 +1,113 @@
+use std::collections::HashMap;
+#[derive(Debug, Clone)]
+struct Scratchcard {
+    card_number: u32,
+    processed: bool,
+    wins: u32,
+}
+
+fn add_cards(n: u32, cards: &HashMap<u32, u32>) -> u32 {
+    let mut result = 0;
+
+    for i in 1..=cards[&n] {
+        if cards.contains_key(&(i as u32)) {
+            result += add_cards(n + i, cards);
+        }
+    }
+
+    cards[&n]
+}
+
+fn main() {
+    let input = std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/ex.txt")).unwrap();
+
+    let mut result = 0;
+    let mut scratchcards = Vec::new();
+    let mut scratchcards_wins = HashMap::<u32, u32>::new();
+
+    for line in input.lines() {
+        let start = line.find(':').unwrap() + 2;
+        let card_number = line[start - 3..start - 2].trim().parse::<u32>().unwrap();
+        // let card_number = line[start - 5..start - 2].trim().parse::<u32>().unwrap();
+        let mut winning = Vec::new();
+        let mut numbers = Vec::new();
+        let mut winning_numbers = true;
+
+        let mut i = start;
+        while i < line.len() - 1 {
+            let number = &line[i..i + 2];
+
+            if number.contains('|') {
+                i += 2;
+                winning_numbers = false;
+                continue;
+            }
+
+            let number = number.trim().parse::<u32>().unwrap();
+
+            if winning_numbers {
+                winning.push(number);
+            } else {
+                numbers.push(number);
+            }
+
+            i += 3;
+        }
+
+        let mut wins: u32 = 0;
+        for number in &numbers {
+            if winning.contains(number) {
+                wins += 1;
+            }
+        }
+
+        if wins > 0 {
+            result += 2_u32.pow(wins - 1);
+        }
+
+        scratchcards.push(Scratchcard {
+            card_number,
+            processed: false,
+            wins,
+        });
+
+        scratchcards_wins.insert(card_number, wins);
+    }
+
+    /*
+    let mut i = 0;
+    while !scratchcards.iter().all(|s| s.processed) {
+        if scratchcards[i].processed {
+            continue;
+        }
+
+        scratchcards[i].processed = true;
+
+        for j in 1..=scratchcards[i].wins {
+            if let Some(card) = scratchcards
+                .iter()
+                .find(|c| c.card_number == scratchcards[i].card_number + j)
+            {
+                scratchcards.push(Scratchcard {
+                    processed: false,
+                    ..(*card).clone()
+                });
+            }
+        }
+        println!("{i} {}", scratchcards.len());
+
+        if i == scratchcards.len() {
+            break;
+        }
+        i = (i + 1) % scratchcards.len();
+    }
+    */
+
+    let mut result_p2 = 0;
+    for (card_number, _) in scratchcards_wins.iter() {
+        result_p2 += add_cards(*card_number, &scratchcards_wins);
+    }
+
+    println!("Part 1: {result}");
+    println!("Part 2: {}", result_p2);
+}
