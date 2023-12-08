4f37acab6de9410c3a8acee0ae85958ae2d2878b
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2023-12-08 14:12:45 +0100

    2023 day 08 part 2 optimized

--- 2023/08/src/main.rs
+++ 2023/08/src/main.rs
@@ -1,9 +1,35 @@
+use num::integer::lcm;
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 struct Node {
     left: String,
     right: String,
+}
+
+fn find_nb_instructions(
+    nodes: &HashMap<String, Node>,
+    instructions: &str,
+    start_node: &str,
+) -> u64 {
+    let mut current_node = start_node.to_string();
+    let mut result = 0;
+    let mut i = 0;
+
+    while !current_node.ends_with('Z') {
+        let instruction = instructions.chars().nth(i).unwrap();
+
+        current_node = match instruction {
+            'R' => nodes[&current_node].right.clone(),
+            'L' => nodes[&current_node].left.clone(),
+            _ => unreachable!(),
+        };
+
+        result += 1;
+        i = (i + 1) % instructions.len();
+    }
+
+    result
 }
 
 fn main() {
@@ -16,14 +42,9 @@ fn main() {
     let mut nodes = HashMap::<String, Node>::new();
 
     for line in lines {
-        let mut split = line.split("=");
+        let mut split = line.split('=');
         let origin = split.next().unwrap().trim();
-        let left_right = split
-            .next()
-            .unwrap()
-            .trim()
-            .replace("(", "")
-            .replace(")", "");
+        let left_right = split.next().unwrap().trim().replace(['(', ')'], "");
         let split = left_right.split(", ").collect::<Vec<&str>>();
 
         nodes.insert(
@@ -35,47 +56,18 @@ fn main() {
         );
     }
 
-    let mut current_node = "AAA".to_string();
-    let mut result_p1 = 0;
-    let mut i = 0;
+    println!(
+        "Part 1: {}",
+        find_nb_instructions(&nodes, instructions, "AAA")
+    );
 
-    while current_node != "ZZZ".to_string() {
-        let instruction = instructions.chars().nth(i).unwrap();
-
-        println!("{current_node}");
-        current_node = match instruction {
-            'R' => nodes[&current_node].right.clone(),
-            'L' => nodes[&current_node].left.clone(),
-            _ => unreachable!(),
-        };
-
-        result_p1 += 1;
-        i = (i + 1) % instructions.len();
-    }
-
-    let mut current_nodes = nodes
-        .iter()
-        .map(|(k, _)| k.clone())
-        .filter(|k| k.ends_with("A"))
-        .collect::<Vec<String>>();
-    let mut result_p2 = 0;
-    let mut i = 0;
-
-    while !current_nodes.iter().all(|k| k.ends_with("Z")) {
-        let instruction = instructions.chars().nth(i).unwrap();
-
-        for node in &mut current_nodes {
-            *node = match instruction {
-                'L' => nodes[node].left.clone(),
-                'R' => nodes[node].right.clone(),
-                _ => unreachable!(),
-            }
-        }
-
-        result_p2 += 1;
-        i = (i + 1) % instructions.len();
-    }
-
-    println!("Part 1: {result_p1}");
-    println!("Part 2: {result_p2}");
+    println!(
+        "Part 2: {}",
+        nodes
+            .keys()
+            .filter(|k| k.ends_with('A'))
+            .fold(1, |result, node| {
+                lcm(result, find_nb_instructions(&nodes, instructions, node))
+            })
+    );
 }
