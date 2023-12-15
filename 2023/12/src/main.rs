9ac9ff4450e1758f6c64ca8c956ae952eb42fe92
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2023-12-15 11:59:35 +0100

    2023 day 12 part 2

--- 2023/12/src/main.rs
+++ 2023/12/src/main.rs
@@ -1,58 +1,59 @@
-use itertools::Itertools;
+use memoize::memoize;
 
-fn find_valid_combinations(mut springs_line: String, groups: Vec<usize>) -> u64 {
-    let mut result = 0;
+#[memoize]
+fn compute_arrangements(line: String, springs_groups: Vec<usize>) -> u64 {
+    if springs_groups.is_empty() {
+        if !line.contains('#') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-    let unknown_parts_indices = springs_line
-        .chars()
-        .enumerate()
-        .map(|(i, c)| if c == '?' { Some(i) } else { None })
-        .filter(|i| i.is_some())
-        .flatten()
-        .collect::<Vec<usize>>();
+    if line == "" {
+        return 0;
+    }
 
-    for combinations in 0..2_u64.pow(unknown_parts_indices.len() as u32) {
-        for (i, &index) in unknown_parts_indices.iter().enumerate() {
-            springs_line.replace_range(
-                index..index + 1,
-                if (combinations >> i) & 1 == 1 {
-                    "#"
-                } else {
-                    "."
-                },
+    let next_character = line.chars().nth(0).unwrap();
+    let next_group = springs_groups[0];
+
+    let empty = || compute_arrangements(line[1..].to_string(), springs_groups.clone());
+
+    let spring = || {
+        if next_group > line.len() {
+            return 0;
+        }
+
+        let current_group = line[..next_group].to_string().replace('?', "#");
+
+        if current_group != "#".repeat(next_group) {
+            return 0;
+        }
+
+        if line.len() == next_group {
+            if springs_groups.len() == 1 {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if "?.".contains(line.chars().nth(next_group).unwrap()) {
+            return compute_arrangements(
+                line[next_group + 1..].to_string(),
+                springs_groups[1..].to_vec(),
             );
         }
 
-        if is_line_valid(&springs_line, &groups) {
-            result += 1;
-        }
-    }
-
-    result
-}
-
-fn is_line_valid(line: &str, springs_groups: &[usize]) -> bool {
-    let mut is_valid = true;
-
-    let groups = line
-        .chars()
-        .group_by(|c| *c == '#')
-        .into_iter()
-        .filter(|(k, _)| *k)
-        .map(|(_, g)| g.collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
-
-    if groups.len() != springs_groups.len() {
-        return false;
+        return 0;
     };
 
-    for (i, group) in groups.iter().enumerate() {
-        if group.len() != springs_groups[i] {
-            is_valid = false;
-        }
+    match next_character {
+        '.' => empty(),
+        '#' => spring(),
+        '?' => empty() + spring(),
+        _ => unreachable!(),
     }
-
-    is_valid
 }
 
 fn main() {
@@ -60,9 +61,7 @@ fn main() {
     let mut result_p1 = 0;
     let mut result_p2 = 0;
 
-    for (i, line) in input.lines().enumerate() {
-        println!("{i}");
-
+    for line in input.lines() {
         let mut parts = line.split_whitespace();
 
         let springs_line = parts.next().unwrap().to_string();
@@ -76,18 +75,17 @@ fn main() {
         let mut springs_line_p2 = springs_line.clone();
         let mut groups_p2 = groups.clone();
 
-        for _ in 0..5 {
+        for _ in 0..4 {
             springs_line_p2.push('?');
             springs_line_p2.push_str(&springs_line);
 
             groups_p2.append(&mut groups.clone());
         }
 
-        result_p1 += find_valid_combinations(springs_line, groups);
-        result_p2 += find_valid_combinations(springs_line_p2, groups_p2);
+        result_p1 += compute_arrangements(springs_line, groups);
+        result_p2 += compute_arrangements(springs_line_p2, groups_p2);
     }
 
-    // 6964: too low
     println!("Part 1: {result_p1}");
     println!("Part 2: {result_p2}");
 }
